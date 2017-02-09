37fcb1bfc89a799526d29b11032aeba0d7def456
Ashish Patel <v-ashisp@microsoft.com>
2017-02-09 10:22:54 -0800

    transcript needs to be more friendly

--- azure_media_services/static/js/player.js
+++ azure_media_services/static/js/player.js
@@ -25,7 +25,13 @@ function AzureMediaServicesBlock(runtime, element) {
 
     this.addEventListener(amp.eventName.pause,
       function(evt){
-        _sendPlayerEvent(
+
+//
+var $xYz = $('.azure-media-player-transcript-pane');
+var iNdex = $(".azure-media-xblock-transcript-element.active").attr("data-transcript-element-id");
+
+console.log("begining",iNdex)
+ _sendPlayerEvent(
           eventPostUrl,
           'edx.video.paused',
           {}
@@ -54,6 +60,33 @@ function AzureMediaServicesBlock(runtime, element) {
 
     this.addEventListener(amp.eventName.loadeddata,
       function(evt) {
+		  var divContainer = $("<div class='azure-media-player-toggle-button-style fa fa-quote-left' id='toggleTranscript' role='button' aria-live='polite' tabindex='0'><div class='vjs-control-content'><span class='vjs-control-text'>Toggle</span></div></div>");
+          	  $(".amp-controlbaricons-right").append(divContainer);
+		  $('.azure-media-player-transcript-pane').hide();
+		  $('.amp-big-play-centered').addClass('azure-media-player-max-screen-width');
+		  $('.xblock-render').addClass('azure-media-player-panel-height');
+		  $('.vjs-has-started').addClass('azure-media-player-max-screen-width');
+		  $('#toggleTranscript').click(function() {
+			$('.azure-media-player-transcript-pane').toggle();
+			var transcriptContainerVisibility = $('.azure-media-player-transcript-pane')[0].style.display;
+			var event_type = ''
+			if(transcriptContainerVisibility === "none"){
+				event_type = 'edx.video.transcript.hidden';
+				$('.xblock-render').addClass('azure-media-player-panel-height');
+				$('.vjs-has-started').removeClass('azure-media-player-min-screen-width');
+				$('.vjs-has-started').addClass('azure-media-player-max-screen-width');
+			} else if(transcriptContainerVisibility === "block"){
+				event_type = 'edx.video.transcript.show';
+				$('.xblock-render').removeClass('azure-media-player-panel-height');
+				$('.vjs-has-started').removeClass('azure-media-player-max-screen-width');
+				$('.vjs-has-started').addClass('azure-media-player-min-screen-width');
+			}
+			_sendPlayerEvent(
+				  eventPostUrl,
+				  event_type,
+				  {}
+				);
+		  });
         _sendPlayerEvent(
           eventPostUrl,
           'edx.video.loaded',
@@ -84,10 +117,13 @@ function AzureMediaServicesBlock(runtime, element) {
         }
       }
     );
+	
 
     transcriptPaneEl = $(element).find('.azure-media-player-transcript-pane');
 
     if (transcriptPaneEl.length) {
+	 
+
       var xhr = new XMLHttpRequest();
       xhr.open('GET', transcriptPaneEl.data('transcript-url'));
       xhr.onreadystatechange = function() {
@@ -97,12 +133,7 @@ function AzureMediaServicesBlock(runtime, element) {
       };
       xhr.send();
     }
-
-
-    $(".vjs-subtitles-button").after(
-  '<div aria-pressed="false" aria-label="Subtitles Menu" aria-haspopup="true" tabindex="0" aria-live="polite" role="button" class="vjs-subtitles-button vjs-menu-button vjs-control  amp-subtitles-control"><div class="vjs-control-content"><span class="vjs-control-text">Subtitles</span><div style="display: none;" class="vjs-menu"><ul class="vjs-menu-content"><li aria-selected="true" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item vjs-selected">Off</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">english</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">spanish</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">french</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">italian</li><li class="amp-menu-header">Subtitles</li></ul></div></div></div>'
-      );
-	  
+  
 	  // Sink events when the user clicks to show a subtitle.
       // NOTE that we are sinking events from the Azure Media Player, so
       // this could change over time (i.e. class names changing)
@@ -125,6 +156,16 @@ function AzureMediaServicesBlock(runtime, element) {
           }
         )
       });
+	  
+    //
+    // Some experimental code here, that we'll disable, but keep around for future reference
+    //
+    if (false) {
+		$(".vjs-subtitles-button").after(
+		'<div aria-pressed="false" aria-label="Subtitles Menu" aria-haspopup="true" tabindex="0" aria-live="polite" role="button" class="vjs-subtitles-button vjs-menu-button vjs-control  amp-subtitles-control"><div class="vjs-control-content"><span class="vjs-control-text">Subtitles</span><div style="display: none;" class="vjs-menu"><ul class="vjs-menu-content"><li aria-selected="true" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item vjs-selected">Off</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">english</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">spanish</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">french</li><li aria-selected="false" tabindex="0" aria-live="polite" role="button" class="vjs-menu-item">italian</li><li class="amp-menu-header">Subtitles</li></ul></div></div></div>'
+		);
+		
+	}
   });
 }
 
@@ -147,18 +188,28 @@ function initTranscript(player, transcript, transcriptPaneEl) {
   parser.parse(transcript);
   parser.flush();
 
-  var html = '<ul class="azure-media-xblock-transcript-cues">';
+  var html = '<ul id="naam" class="azure-media-xblock-transcript-cues"><li role="link" style="height:170px;"><span style="height:170px;"></span></li>';
   for(var i=0;i<cues.length;i++) {
     var cue = cues[i];
-    html += '<li class="azure-media-xblock-transcript-cue"><span class="azure-media-xblock-transcript-element" data-transcript-element-id=' +
+    html += '<li id="'+cue.id+'" class="azure-media-xblock-transcript-cue"><span class="azure-media-xblock-transcript-element" data-transcript-element-id=' +
         cue.id + ' data-transcript-element-start-time="' + cue.startTime + '" >' +
         cue.text + '</span></li>';
   }
-  html += '</ul>';
+  html += '<li role="link" style="height:170px;"><span style="height:170px;"></span></li></ul>';
   transcriptPaneEl.append(html);
 
-  // handle events when user clicks on transcripts
+ // handle events when user clicks on transcripts
   $('.azure-media-xblock-transcript-element').click(function(evt){
+var datatranscriptelementid = $(this).attr('data-transcript-element-id');
+        var targetEl = $('span[data-transcript-element-id='+datatranscriptelementid+']');
+      var isActive = targetEl.hasClass('active');
+if (!isActive) {
+        // highlight the correct one
+        $('.azure-media-xblock-transcript-element').removeClass('active');
+        targetEl.addClass('active');
+}
+
+//
     var start_time = parseFloat($(evt.target).data('transcript-element-start-time'));
 
     // set the player to match the transcript time
@@ -192,16 +243,27 @@ function _syncTimer(player, transcript_cues, element) {
         // highlight the correct one
         $('.azure-media-xblock-transcript-element').removeClass('active');
         targetEl.addClass('active');
+		 // after getting highlighted one, below one wil autoscroll.
+		var topPositionOfActiveElement = targetEl.position().top;
+                var transcriptPanelVisibleAreaHeight = transcriptPaneEl[0].clientHeight;
+                var halfOfTranscriptPanelContainer = transcriptPanelVisibleAreaHeight / 2;
+                if(topPositionOfActiveElement!==0 && 
+		   topPositionOfActiveElement > halfOfTranscriptPanelContainer &&
+		   topPositionOfActiveElement>transcriptPanelVisibleAreaHeight) {
+			var newScrollTopPosition = Math.ceil(transcriptPanelVisibleAreaHeight/6);
+			$('.azure-media-player-transcript-pane')[0].scrollTop += newScrollTopPosition;
+		}
       }
-      return;
+     return;
     }
   }
 
   // clear all - video is not currently at a point which has a current
   // translation
   $('.azure-media-xblock-transcript-element').removeClass('active');
-}
 
+}
+  
 function _sendPlayerEvent(eventPostUrl, name, data) {
   data['event_type'] = name;
 
