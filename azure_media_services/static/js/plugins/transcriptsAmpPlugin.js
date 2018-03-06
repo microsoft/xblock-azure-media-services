2a3ac95ab0627d971c03760378dda148dc43aceb
wowkalucky <wowkalucky@gmail.com>
2018-03-06 13:03:39 +0200

    Update Transcripts Plugin

--- azure_media_services/static/js/plugins/transcriptsAmpPlugin.js
+++ azure_media_services/static/js/plugins/transcriptsAmpPlugin.js
@@ -8,65 +8,66 @@
 
     var transcriptCues = null;
 
+    var Component = amp.getComponent('Component');
     var MenuItem = amp.getComponent('MenuItem');
+    var MenuButton = amp.getComponent('MenuButton');
+
     var TranscriptsMenuItem = amp.extend(MenuItem, {
         constructor: function() {
-            var player = arguments[0];
+            var player = arguments[0];  // eslint-disable-line no-unused-vars
             var options = arguments[1];
             this.track = options.track;
+            this.cues = [];
             options.label = options.label || this.track.label || 'Unknown';
             MenuItem.apply(this, arguments);
         },
-        handleClick: function(evt) {
-            var $transcriptButtonMenu = $(this.el()).closest('.vjs-transcripts-button');
-            var $target = $(evt.target);
+        handleClick: function(evt) {  // eslint-disable-line no-unused-vars
             var player = this.player();
             var $wrapper = $('div.tc-wrapper');
-            var $transcriptElement = $('div.tc-container');
+            var $transcriptContainer = $('div.tc-container');
 
-            $transcriptButtonMenu
-                .find('.vjs-menu-item')
-                .removeClass('vjs-selected')
-                .attr('aria-selected', false);
-            $target
-                .addClass('vjs-selected')
-                .attr('aria-selected', true);
+            this.options_.parent.items.forEach(function(item) {  // eslint-disable-line no-underscore-dangle
+                item.selected(false);
+            });
+            this.selected(true);
 
-            if ($.trim($target.text()) === 'Off, selected') {
+            if (this.options_.identity === 'off') {  // eslint-disable-line no-underscore-dangle
                 $wrapper.addClass('closed');
             } else {
-                transcriptCues = initTranscript(player, $transcriptElement, this.track);
+                transcriptCues = initTranscript(  // eslint-disable-line no-use-before-define
+                    player, $transcriptContainer, this.track
+                );
                 $wrapper.removeClass('closed');
             }
         }
     });
 
-    var MenuButton = amp.getComponent('MenuButton');
     var TranscriptsMenuButton = amp.extend(MenuButton, {
         constructor: function() {
             MenuButton.apply(this, arguments);
             this.addClass('vjs-transcripts-button');
             this.addClass('fa');
             this.addClass('fa-quote-left');
-            if (arguments && arguments[1].identity === 'off') {
-                this.el().classList.add('off');
-            }
         },
         createItems: function() {
             var player = this.player();
             var items = [];
+            var menuButton = this;
             var tracks = player.textTracks();
             if (!tracks) {
                 return items;
             }
             items.push(new TranscriptsMenuItem(player, {
-                selectable: true,
-                selected: true,
+                identity: 'off',
                 label: 'Off',
-                identity: 'off'
+                parent: menuButton,
+                selectable: true,
+                selected: true
             }));
-            items = items.concat(tracks.tracks_.map(function(track) {
+            items = items.concat(tracks.tracks_.map(function(track) {  // eslint-disable-line no-underscore-dangle
                 return new TranscriptsMenuItem(player, {
+                    identity: 'item',
+                    parent: menuButton,
                     selectable: true,
                     track: track
                 });
@@ -74,34 +75,113 @@
             return items;
         }
     });
+
+    var MainContainer = amp.extend(Component, {
+        constructor: function() {
+            Component.apply(this, arguments);
+            this.addClass('tc-wrapper');
+            this.addClass('video');
+            this.addClass('closed');
+        }
+    });
+
+    var TranscriptContainer = amp.extend(Component, {
+        constructor: function() {
+            Component.apply(this, arguments);
+        },
+        createEl: function() {
+            return $('<div class="tc-container"><ul class="subtitles-menu"></ul></div>').get(0);
+        }
+    });
+
+    var CueItem = amp.extend(MenuItem, {
+        constructor: function() {
+            var player = arguments[0];  // eslint-disable-line no-unused-vars
+            var options = arguments[1];
+            this.text = options.text;
+            this.startTime = options.startTime;
+            this.endTime = options.endTime;
+            MenuItem.apply(this, arguments);
+        },
+        createEl: function() {
+            return Component.prototype.createEl(
+                'li',
+                {
+                    tabIndex: -1,
+                    role: 'link',
+                    className: 'transcript-cue',
+                    innerHTML: $('<span>').text(this.options_.text).html()  // eslint-disable-line no-underscore-dangle
+                },
+                {
+                    'data-cue-start': this.options_.startTime  // eslint-disable-line no-underscore-dangle
+                }
+            );
+        }
+    });
+
     amp.registerComponent('TranscriptsMenuButton', TranscriptsMenuButton);
+    amp.plugin('transcriptsAmpPlugin', function() {
+        var player = this;
+        var timeHandler = null;
+        var $vidParent = $(player.el()).parent().parent();
+        var tcButton = new TranscriptsMenuButton(player, {title: 'TRANSCRIPTS'});
+        var mainContainer = new MainContainer(player, {});
+        var transcriptContainer = new TranscriptContainer(player, {});
+        var $transcriptContainerEl = $(transcriptContainer.el());
 
+        this.addEventListener('loadeddata', function() {
+            $vidParent.wrap(mainContainer.el());
+            $vidParent.parent().append(transcriptContainer.el());
 
-    /**
+            player
+                .getChild('controlBar')
+                .getChild('controlBarIconsRight')
+                .addChild(tcButton);
+        });
+        this.addEventListener(amp.eventName.play, function(evt) {  // eslint-disable-line no-unused-vars
+            timeHandler = setInterval(function() {
+                syncTimer(player, transcriptCues, $transcriptContainerEl);  // eslint-disable-line no-use-before-define
+            },
+            100
+            );
+        });
+        this.addEventListener(amp.eventName.pause, function(evt) {  // eslint-disable-line no-unused-vars
+            if (timeHandler !== null) {
+                clearInterval(timeHandler);
+            }
+        });
+        this.addEventListener(amp.eventName.ended, function(evt) {  // eslint-disable-line no-unused-vars
+            if (timeHandler !== null) {
+                clearInterval(timeHandler);
+            }
+        });
+    });
+
+     /**
      * This is called regularly while the video plays
      * so that we can correctly highlight the transcript elements
      * based on the current position of the video playback
      * @param player
-     * @param transcriptCues
+     * @param trackCues
      * @param $transcriptElement
      * @private
      */
-    function syncTimer(player, transcriptCues, $transcriptElement) {
+    function syncTimer(player, trackCues, $transcriptElement) {
         // Gather each transcript phrase (each pseudo-hyperlink in transcript).
         var cue;
         var isActive;
         var $targetElement;
         var scrollUpSize;
-        var $transcriptItems = $transcriptElement.find('.azure-media-xblock-transcript-element');
+        var $transcriptItems = $transcriptElement.find('.transcript-cue');
         var currentTime = player.currentTime();
 
-        if (transcriptCues === null || !$transcriptElement.length) {
+        if (trackCues === null || !$transcriptElement.length) {
             return;
         }
 
         // Simple linear search.
-        for (var i = 0; i < transcriptCues.length; i++) {  // eslint-disable-line vars-on-top
-            cue = transcriptCues[i];
+        for (var i = 0; i < trackCues.length; i++) {  // eslint-disable-line vars-on-top
+            cue = trackCues[i];
 
             if (currentTime >= cue.startTime && currentTime < cue.endTime) {
                 $targetElement = $transcriptItems.eq(i);
@@ -125,37 +205,34 @@
         }
     }
 
-
     /**
      * Transcripts creating.
      * @param player
-     * @param transcript
      * @param $transcriptElement
+     * @param track
      * @returns {Array}
      */
     function initTranscript(player, $transcriptElement, track) {
         var cue;
-        var html;
+        var cueComponent;
         var startTime;
         var $transcriptItems;
         var cues = track.cues;
+        var $html = $('<ul class="subtitles-menu"></ul>');
 
-        // Creates transcript markup.
-        // TODO: use Backbone's client-side templating view (underscore)
-        html = '<ol class="subtitles-menu" style="list-style:none;">';
         for (var i = 0; i < cues.length; i++) { // eslint-disable-line vars-on-top
             cue = cues[i];
-
-            html += '<li role="link" tabindex="0"'
-                + 'data-transcript-element-start-time="' + _.escape(cue.startTime)
-                + '" class="azure-media-xblock-transcript-element" >'
-                + _.escape(cue.text) + '</li>';
+            cueComponent = new CueItem(player, {
+                text: cue.text,
+                startTime: cue.startTime,
+                endTime: cue.endTime
+            });
+            $html.append(cueComponent.el());
         }
-        html += '</ol>';
-        $transcriptElement.html(html);
+        $transcriptElement.html($html);
 
         // Gather each transcript phrase (each pseudo-hyperlink in transcript).
-        $transcriptItems = $transcriptElement.find('.azure-media-xblock-transcript-element');
+        $transcriptItems = $transcriptElement.find('.transcript-cue');
 
         // Handle events when user clicks on transcripts
         $transcriptItems.on('click keypress', function(evt) {
@@ -174,49 +251,10 @@
             $(evt.target).addClass('current');
 
             // Set the player to match the transcript time
-            startTime = parseFloat($(evt.target).data('transcript-element-start-time'));
+            startTime = parseFloat($(evt.target).data('cue-start'));
             player.currentTime(startTime);
         });
 
         return cues;
     }
-
-    amp.plugin('transcriptsAmpPlugin', function() {
-        var player = this;
-        var timeHandler = null;
-        var $vidParent = $(player.el()).parent();
-        var $transcriptElement = null;
-
-        this.addEventListener('loadeddata', function() {
-            var $vidAndTranscript = $(
-                '<div class="tc-wrapper video closed"><div class="azuremediaplayer"></div></div>'
-            );
-            $transcriptElement = $('<div class="tc-container"></div>');
-            $vidParent.wrap($vidAndTranscript);
-            $transcriptElement.appendTo($('.tc-wrapper'));
-
-            player
-                .getChild('controlBar')
-                .getChild('controlBarIconsRight')
-                .addChild('TranscriptsMenuButton', {title: 'TRANSCRIPTS'});
-
-        });
-        this.addEventListener(amp.eventName.play, function(evt) {  // eslint-disable-line no-unused-vars
-            timeHandler = setInterval(function() {
-                syncTimer(player, transcriptCues, $transcriptElement);
-            },
-            100
-            );
-        });
-        this.addEventListener(amp.eventName.pause, function(evt) {  // eslint-disable-line no-unused-vars
-            if (timeHandler !== null) {
-                clearInterval(timeHandler);
-            }
-        });
-        this.addEventListener(amp.eventName.ended, function(evt) {  // eslint-disable-line no-unused-vars
-            if (timeHandler !== null) {
-                clearInterval(timeHandler);
-            }
-        });
-    });
 }).call(this);
